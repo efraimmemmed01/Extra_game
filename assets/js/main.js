ef827456ab2e9662413372a0e3b5beb0d0d4da70
sekineibadli-source <sekineibadli@gmail.com>
2026-02-18 14:34:33 +0400

    shop elave olundu

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+
